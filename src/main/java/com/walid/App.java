e9cf453a00fbf472bbc74dbfa430cf99cc1ff1d3
Walid Moustafa <walid.moustafa@alexsolutions.com.au>
2017-08-19 21:15:51 +1000

    Implement making anagrams.

--- src/main/java/com/walid/App.java
+++ src/main/java/com/walid/App.java
@@ -9,9 +9,15 @@ import java.util.stream.IntStream;
 public class App {
 
     public static void main(String[] args) {
+
         leftRotate();
+
+        makeAnagram();
     }
 
+    /**
+     * Given an array of  integers and a number, , perform  left rotations on the array. Then print the updated array as a single line of space-separated integers.
+     */
     public static void leftRotate() {
 
         Scanner in = new Scanner(System.in);
@@ -35,5 +41,49 @@ public class App {
                 .boxed()
                 .map(x -> String.valueOf(x))
                 .collect(Collectors.joining(" ")));
+    }
+
+
+    /**
+     * Given two strings,  and , that may or may not be of the same length, determine the minimum number of character deletions required to make  and  anagrams. Any characters can be deleted from either of the strings.
+     */
+    public static void makeAnagram() {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        System.out.println(numberNeeded(a, b));
+    }
+
+    private static long numberNeeded(String first, String second) {
+
+        int needeNumber = 0;
+
+        String firstMutable = first;
+        String secondMutable = second;
+
+
+        if (first == null || first.length() == 0) {
+            return second.length();
+        } else if (second == null || second.length() == 0) {
+            return first.length();
+        }
+
+        for (char c : first.toCharArray()) {
+            if (secondMutable.indexOf(c) == -1) {
+                needeNumber++;
+            } else {
+                secondMutable = secondMutable.replaceFirst(String.valueOf(c), "");
+            }
+        }
+
+        for (char c : second.toCharArray()) {
+            if (firstMutable.indexOf(c) == -1) {
+                needeNumber++;
+            } else {
+                firstMutable = firstMutable.replaceFirst(String.valueOf(c), "");
+            }
+        }
+
+        return needeNumber;
     }
 }
